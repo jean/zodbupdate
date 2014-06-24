3a84d756e778bb778e52df672312e4500c1b2ff3
Jean Jordaan <jean.jordaan@gmail.com>
2014-06-24 14:33:56 +0700

    Rename README

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='zodbupdate',
       description=
         'Update ZODB class references for moved or renamed classes.',
       long_description=(
-        read('README.txt')
+        read('README.md')
         + '\n' +
         read('CHANGES.txt')),
       version='0.6dev',
